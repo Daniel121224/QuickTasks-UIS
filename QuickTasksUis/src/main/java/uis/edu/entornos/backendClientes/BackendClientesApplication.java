9b3e2539b374674d0a3e19a9a82bd1b28053b112
Daniel Robles <jorge2210065@correo.uis.edu.co>
2024-04-09 21:48:05 -0500

    Codigo

---
+++ QuickTasksUis/src/main/java/uis/edu/entornos/backendClientes/BackendClientesApplication.java
@@ -0,0 +1,13 @@
+package uis.edu.entornos.backendClientes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendClientesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendClientesApplication.class, args);
+	}
+
+}
---
+++ QuickTasksUis/src/test/java/uis/edu/entornos/backendClientes/BackendClientesApplicationTests.java
@@ -0,0 +1,13 @@
+package uis.edu.entornos.backendClientes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendClientesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
